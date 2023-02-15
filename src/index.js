9bfa9947b39777e784b11a9873c8748acfadb69c
YeissenMacalupu <73966769@certus.edu.pe>
2023-02-15 11:12:10 -0500

    nueva forma

--- src/index.js
+++ src/index.js
@@ -1,7 +1,40 @@
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+//RUTAS
+app.get('/', (req, res) => {
+    res.send('Bienvenido a la API REST')
+});
+
+mongoose.set('strictQuery', true);
+
+
+//CONECTANDO A MONGO DB ATLAS
+
+mongoose.connect(process.env.mongodb_conexion)
+    .then(() => console.log("Conexion correcta a MongoDB Atlas"))
+    .catch((error) => console.error(error));
+
+
+//COMPROBAMOS QUE ESTE ESCUCHANDO EL PUERTO
+app.listen(port, () => console.log('Servidor funcionando en el puerto', port));
+
+
+
+
+
+
+
+
+
 //Requieres
 
 // const { append } = require("express/lib/response");
-const express = require("express");
+/*const express = require("express");
 var mongoose = require("mongoose");
 
 
@@ -16,4 +49,4 @@ mongoose.connect(process.env.mongodb_conexion)
     .catch((error) => console.log(error))
 
 /*Comprovamos si el servidor funciona*/
-app.listen(3000, () => console.log("Servidor funcionando en el puerto", port));
+// app.listen(3000, () => console.log("Servidor funcionando en el puerto", port));
